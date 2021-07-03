2e94bf985af72a8dc0f4460fa4c652c7ef493615
tabosa <tiaggo95@hotmail.com>
2021-07-03 18:55:17 -0300

    add react router, routes and components

---
+++ app/javascript/components/Airlines/Airlines.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Airlines = () => {
+  return <div>This is the Airlines#index view of our app.</div>
+}
+
+export default Airlines
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
     end
   end
 
-  get '#path', to: 'pages#index', via: :all
+  get '*path', to: 'pages#index', via: :all
 end
